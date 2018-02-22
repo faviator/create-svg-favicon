0a859b085066f374f85ccec93e1c4814f97ba229
Jason Yu <me@ycmjason.com>
2018-02-22 21:29:38 +0000

    exporting faviatorIconConfig and use a more predictable default config values, also refactored tests

--- createSvgFavicon.js
+++ createSvgFavicon.js
@@ -1,17 +1,17 @@
 const createSvgFavicon = ({
   size = 16,
-  text = 'F',
+  text = '',
   dx = 0,
   dy = 0,
-  fontSize = 80,
-  fontFamily = 'Dancing Script',
-  fontColor = 'white',
-  backgroundColor = 'rgb(219, 59, 211)',
-  borderWidth = 5,
-  borderColor = '#0D1423',
-  borderRadius,
-  rx = '5%',
-  ry = '5%',
+  fontSize = 0,
+  fontFamily = '',
+  fontColor = '',
+  backgroundColor = '',
+  borderWidth = 0,
+  borderColor = '',
+  borderRadius = 0,
+  rx = 0,
+  ry = 0,
 } = {}) => `<?xml version="1.0" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size}" viewBox="0 0 100 100">
@@ -52,3 +52,4 @@ const createSvgFavicon = ({
 
 module.exports = createSvgFavicon;
 module.exports.default = createSvgFavicon;
+module.exports.faviatorIconConfig = require('./faviatorIconConfig');
--- test/createSvgFavicon.test.js
+++ test/createSvgFavicon.test.js
@@ -1,7 +1,91 @@
 const assert = require('assert');
 
+const faviatorIconConfig = require('../faviatorIconConfig');
 const createSvgFavicon = require('../createSvgFavicon');
-const removeNewLines = (s) => s.replace(/(\r|\n)/g, '');
+
+const assertFavicon = (svg) => {
+  svg = svg.replace(/(\r|\n)/g, '');
+  assert.equal(svg.match(new RegExp(`<text [^>]*?text-anchor="([^"]*)"`, 'i'))[1], 'middle');
+  assert.equal(svg.match(new RegExp(`<text [^>]*?alignment-baseline="([^"]*)"`, 'i'))[1], 'central');
+  return {
+    size(size) {
+      assert.equal(svg.match(/<svg [^>]*?height="([^"]*)"/i)[1], String(size));
+      assert.equal(svg.match(/<svg [^>]*?width="([^"]*)"/i)[1], String(size));
+      return this;
+    },
+
+    text(text) {
+      assert.equal(svg.match(new RegExp(`<text.*?>([^<]*)</text>`, 'i'))[1], String(text));
+      return this;
+    },
+
+    dx(dx) {
+      assert.equal(svg.match(new RegExp(`<text [^>]*?dx="([^"]*)"`, 'i'))[1], String(dx));
+      return this;
+    },
+
+    dy(dy) {
+      assert.equal(svg.match(new RegExp(`<text [^>]*?dy="([^"]*)"`, 'i'))[1], String(dy));
+      return this;
+    },
+
+    fontSize(fontSize) {
+      assert.equal(svg.match(new RegExp(`<text [^>]*?font-size="([^"]*)"`, 'i'))[1], String(fontSize));
+      return this;
+    },
+
+    fontFamily(fontFamily) {
+      assert.equal(svg.match(new RegExp(`<text [^>]*?font-family="([^"]*)"`, 'i'))[1], String(fontFamily));
+      return this;
+    },
+
+    fontColor(fontColor) {
+      assert.equal(svg.match(new RegExp(`<text [^>]*?fill="([^"]*)"`, 'i'))[1], String(fontColor));
+      return this;
+    },
+
+    backgroundColor(backgroundColor) {
+      assert.equal(svg.match(new RegExp(`<rect [^>]*?fill="([^"]*)"`, 'i'))[1], String(backgroundColor));
+      return this;
+    },
+
+    borderRadius(borderRadius) {
+      this.rx(borderRadius);
+      this.ry(borderRadius);
+      return this;
+    },
+
+    borderColor(borderColor) {
+      assert.equal(svg.match(new RegExp(`<rect [^>]*?stroke="([^"]*)"`, 'i'))[1], String(borderColor));
+      return this;
+    },
+
+    borderWidth(borderWidth) {
+      assert.equal(svg.match(new RegExp(`<rect [^>]*?stroke-width="([^"]*)"`, 'i'))[1], String(borderWidth));
+      return this;
+    },
+
+    rx(rx) {
+      assert.equal(svg.match(new RegExp(`<rect [^>]*?rx="([^"]*)"`, 'i'))[1], String(rx));
+      return this;
+    },
+
+    ry(ry) {
+      assert.equal(svg.match(new RegExp(`<rect [^>]*?ry="([^"]*)"`, 'i'))[1], String(ry));
+      return this;
+    },
+
+    x(x) {
+      assert.equal(svg.match(new RegExp(`<text [^>]*?x="([^"]*)"`, 'i'))[1], String(x));
+      return this;
+    },
+
+    y(y) {
+      assert.equal(svg.match(new RegExp(`<text [^>]*?y="([^"]*)"`, 'i'))[1], String(y));
+      return this;
+    },
+  };
+};
 
 describe('createSvgFavicon', function() {
   it('should export default as well for es6', function() {
@@ -12,33 +96,40 @@ describe('createSvgFavicon', function() {
     assert((/<!doctype svg/i).test(createSvgFavicon()));
   });
 
+  it('should generate the faviatorIconConfig properly', function() {
+    const svg = createSvgFavicon(faviatorIconConfig);
+    const assertThisFavicon = assertFavicon(svg);
+    for (let [k, v] of Object.entries(faviatorIconConfig)) {
+      assertThisFavicon[k](v);
+    }
+  });
+
   it('should generate a svg tag with width and height as specified in config.size', function() {
-    const svg = removeNewLines(createSvgFavicon({ size: 100 }));
-    assert.equal(svg.match(/<svg [^>]*?height="([^"]*)"/i)[1], '100');
-    assert.equal(svg.match(/<svg [^>]*?width="([^"]*)"/i)[1], '100');
+    const svg = createSvgFavicon({ size: 100 });
+    assertFavicon(svg).size(100);
   });
 
   it('should generate a rect tag with rx, ry, fill', function() {
     const rx = '50%';
     const ry = '50%';
     const backgroundColor = '#03f53a';
-    const svg = removeNewLines(createSvgFavicon({
+    const svg = createSvgFavicon({
       rx,
       ry,
       backgroundColor,
-    }));
-    assert.equal(svg.match(new RegExp(`<rect [^>]*?rx="([^"]*)"`, 'i'))[1], rx);
-    assert.equal(svg.match(new RegExp(`<rect [^>]*?ry="([^"]*)"`, 'i'))[1], ry);
-    assert.equal(svg.match(new RegExp(`<rect [^>]*?fill="([^"]*)"`, 'i'))[1], backgroundColor);
+    });
+    assertFavicon(svg)
+      .rx(rx)
+      .ry(ry)
+      .backgroundColor(backgroundColor);
   });
 
   it('should generate a rect tag with rx, ry with value borderRadius', function() {
     const borderRadius = '50%';
-    const svg = removeNewLines(createSvgFavicon({
+    const svg = createSvgFavicon({
       borderRadius,
-    }));
-    assert.equal(svg.match(new RegExp(`<rect [^>]*?rx="([^"]*)"`, 'i'))[1], borderRadius);
-    assert.equal(svg.match(new RegExp(`<rect [^>]*?rx="([^"]*)"`, 'i'))[1], borderRadius);
+    });
+    assertFavicon(svg).borderRadius(borderRadius);
   });
 
   it('should generate a text tag with content text, fill, font-family, font-size', function() {
@@ -46,23 +137,20 @@ describe('createSvgFavicon', function() {
     const fontColor = 'red';
     const fontFamily = 'Open Sans';
     const fontSize = 65;
-    const svg = removeNewLines(createSvgFavicon({
+    const svg = createSvgFavicon({
       text,
       fontColor,
       fontFamily,
       fontSize,
-    }));
-    assert.equal(svg.match(new RegExp(`<text.*?>([^<]*)</text>`, 'i'))[1], text);
-    assert.equal(svg.match(new RegExp(`<text [^>]*?fill="([^"]*)"`, 'i'))[1], fontColor);
-    assert.equal(svg.match(new RegExp(`<text [^>]*?font-family="([^"]*)"`, 'i'))[1], fontFamily);
-    assert.equal(svg.match(new RegExp(`<text [^>]*?font-size="([^"]*)"`, 'i'))[1], fontSize);
+    });
+    assertFavicon(svg)
+      .text(text)
+      .fontColor(fontColor)
+      .fontFamily(fontFamily)
+      .fontSize(fontSize);
   });
 
   it('should generate a text tag with at center', function() {
-    const svg = removeNewLines(createSvgFavicon());
-    assert.equal(svg.match(new RegExp(`<text [^>]*?x="([^"]*)"`, 'i'))[1], '50');
-    assert.equal(svg.match(new RegExp(`<text [^>]*?y="([^"]*)"`, 'i'))[1], '50');
-    assert.equal(svg.match(new RegExp(`<text [^>]*?text-anchor="([^"]*)"`, 'i'))[1], 'middle');
-    assert.equal(svg.match(new RegExp(`<text [^>]*?alignment-baseline="([^"]*)"`, 'i'))[1], 'central');
+    assertFavicon(createSvgFavicon()).x(50).y(50);
   });
 });
